76273e47d691a31088e73d1578488223decb5503
yooseek <y1212ks@naver.com>
2022-01-25 22:29:41 +0900

    오류발생

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() { // main thread 는 runApp을 실행시키고 종료됩니다.
@@ -9,72 +11,37 @@ class FirstApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp( // ios는 CupertinoApp
       // AndroidApp 을 만들꺼에요,
-      home: SafeArea(
-        child: Scaffold(
-          body: HomeApp(),
-        ),
+      theme: ThemeData(
+        primaryColor: Colors.white,
       ),
-    );
-  }
-}
-
-class HomeApp extends StatelessWidget {
-  Widget build(BuildContext context){
-    var m = MediaQuery.of(context);
-    var safeArea = m.size.height - AppBar().preferredSize.height - m.padding.top - m.padding.bottom;
-    print("넓이 : ${m.size.width}");
-    print("높이 : ${m.size.height}");
-    print("패딩 탑 : ${safeArea}");
-    var test = AppBar().preferredSize.height;
-    print("앱바 사이즈 : ${test}");
-    return MaterialApp(
       home: SafeArea(
         child: Scaffold(
-          body: Center(
-            child: Column(
-              children: [
-                SizedBox(height: 50,),
-                Image.asset("assets/test.jpg"),
-                SizedBox(height: 40,),
-                Text("NeedLeWork", style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
-                SizedBox(height: 20,),
-                Text("is seek",style: TextStyle(fontSize: 20),),
-                SizedBox(height: 20,),
-                Text("HandCreft"),
-                SizedBox(height: 50,),
-                RaisedButton(
-                  onPressed: (){print('버튼클릭됨');},
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(400)),
-                  child: Text('Get Start',
-                    style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,),
-                  ),
-                  color: Colors.black,
-
-                )
-              ],
+          appBar: AppBar(
+            leading: Icon(Icons.arrow_back),
+            title: const Text("쿠키 Card",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    fontFamily: "CR"),
             ),
+            centerTitle: true,
+            actions: [
+              Row(
+                children: [
+                  Icon(Icons.shopping_cart_rounded),
+                  SizedBox(width: 10,)
+                ],
+              ),
+            ],
           ),
+          body: Column(
+            children: [
+              ClipRRect(borderRadius: BorderRadius.circular(10.0),)
+            ],
           )
+
         ),
-    );
-  }
-
-  Widget buildMaterialApp(int index) {
-    return Column(
-      children: [
-        ListTile(
-        title: Text('제목'),
-        subtitle: Text("부제목"),
-        leading: Icon(Icons.account_box_sharp),
-        onTap: (){
-          print('눌러짐');
-        },
       ),
-      Container(
-      height: 5,
-      color: Colors.red,
-      )]
     );
   }
-
 }
